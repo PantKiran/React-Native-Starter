597963698c6d020a0dd60de7c07ac066e7ef9e2d
kiran pant <kiran.r.pant@gmail.com>
2024-06-11 13:07:23 +0545

    UI:show flatList

--- src/screens/ListScreen.jsx
+++ src/screens/ListScreen.jsx
@@ -1,19 +1,46 @@
-import React from 'react'
-import { Text, View,StyleSheet } from 'react-native'
+import React from "react";
+import { Text, View, StyleSheet, FlatList } from "react-native";
 
 const ListScreen = () => {
+  const friends = [
+    {
+      name: "Friend #1",
+    },
+    {
+      name: "Friend #2",
+    },
+    {
+      name: "Friend #3",
+    },
+    {
+      name: "Friend #4",
+    },
+    {
+      name: "Friend #5",
+    },
+    {
+      name: "Friend #6",
+    },
+    {
+      name: "Friend #7",
+    },
+  ];
   return (
     <View>
-
-        <Text>List Screen</Text>
+      <FlatList
+        data={friends}
+        renderItem={({ item }) => {
+         return <Text>{item.name}</Text>;
+        }}
+      />
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
-    textStyle:{
-        fontSize:30
-    }
-})
+  textStyle: {
+    fontSize: 30,
+  },
+});
 
-export default ListScreen
+export default ListScreen;
