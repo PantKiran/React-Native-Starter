5835fbcda9f41652d98aef7b94047cc60523b801
kiran pant <kiran.r.pant@gmail.com>
2024-06-12 18:03:58 +0545

    UI:additional props

--- src/screens/ListScreen.jsx
+++ src/screens/ListScreen.jsx
@@ -5,41 +5,53 @@ const ListScreen = () => {
   const friends = [
     {
       name: "Friend #1",
+      age: 34,
     },
     {
       name: "Friend #2",
+      age: 35,
     },
     {
       name: "Friend #3",
+      age: 36,
     },
     {
       name: "Friend #4",
+      age: 37,
     },
     {
       name: "Friend #5",
+      age: 38,
     },
     {
       name: "Friend #6",
+      age: 39,
     },
     {
       name: "Friend #7",
+      age: 32,
     },
     {
-        name: "Friend #8",
-      },
-      {
-        name: "Friend #9",
-      },
+      name: "Friend #8",
+      age: 30,
+    },
+    {
+      name: "Friend #9",
+      age: 31,
+    },
   ];
   return (
     <View>
-      <FlatList 
-      horizontal={true}
-      showsHorizontalScrollIndicator={false}
-      keyExtractor={friends=>friends.name}
+      <FlatList
+        showsVerticalScrollIndicator={false}
+        keyExtractor={(friends) => friends.name}
         data={friends}
         renderItem={({ item }) => {
-         return <Text style={styles.textStyle}>{item.name}</Text>;
+          return (
+            <Text style={styles.textStyle}>
+              {item.name} -Age {item.age}
+            </Text>
+          );
         }}
       />
     </View>
@@ -48,7 +60,7 @@ const ListScreen = () => {
 
 const styles = StyleSheet.create({
   textStyle: {
-    marginVertical:50
+    marginVertical: 50,
   },
 });
 
