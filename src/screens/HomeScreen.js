d2069eb627c543cbc9c3b383bb71529b51450939
kiran pant <kiran.r.pant@gmail.com>
2024-06-12 19:27:07 +0545

    UI:Destructuring Props

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,14 +2,17 @@ import React from "react";
 import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
 const HomeScreen = (props) => {
+  const {
+    navigation: { navigate },
+  } = props;
   return (
     <View>
       <Text style={styles.text}>First react native app</Text>
       <Button
         title="Go to Components Demo"
-        onPress={() => props.navigation.navigate("Components")}
+        onPress={() => navigate("Components")}
       />
-      <TouchableOpacity onPress={() => props.navigation.navigate("List")}>
+      <TouchableOpacity onPress={() => navigate("List")}>
         <Text>Go to List Demo</Text>
       </TouchableOpacity>
     </View>
