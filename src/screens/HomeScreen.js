8ae7302e1dc5c32064a96cbdae3c814a1883cffe
kiran pant <kiran.r.pant@gmail.com>
2024-06-12 18:46:11 +0545

    UI:Touchable Opacity

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,12 +1,15 @@
 import React from "react";
-import { Text, StyleSheet} from "react-native";
+import { Text, StyleSheet, View, Button,TouchableOpacity} from "react-native";
 
 const HomeScreen = () => {
   return (
-    <>
-      
+    <View>
       <Text style={styles.text}>First react native app</Text>
-    </>
+      <Button title="Go to Components Demo" onPress={()=>{ navigator('Component')}}/>
+      <TouchableOpacity onPress={()=>console.log("List Pressed")}>
+        <Text>Go to List Demo</Text>
+      </TouchableOpacity>
+    </View>
   );
 };
 
