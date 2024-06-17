9c5887ba6762cd3c33c8e5b4d4a45c6dcaebedfd
kiran pant <kiran.r.pant@gmail.com>
2024-06-17 18:11:39 +0545

    UI:fix

--- src/screens/TextScreen.jsx
+++ src/screens/TextScreen.jsx
@@ -1,24 +1,43 @@
-import React, { useState } from 'react'
-import { Text, TextInput, View ,StyleSheet} from 'react-native'
+import React, { useState } from "react";
+import { Text, TextInput, View, StyleSheet } from "react-native";
 
 const TextScreen = () => {
-   const [name,setName]= useState('')
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
   return (
-   <View>
-    <Text>Enter Name:</Text>
-    <TextInput value={ name} onChangeText={(newValue)=>setName(newValue)} autoCapitalize='none' autoCorrect={false} style={styles.input} />
-    <Text>My Name is: {name}</Text>
-
-   </View>
-  )
-}
+    <View>
+      <Text>Enter Name:</Text>
+      <TextInput
+        value={name}
+        onChangeText={(newValue) => setName(newValue)}
+        autoCapitalize="none"
+        autoCorrect={false}
+        style={styles.input}
+      />
+      <Text>Enter Password:</Text>
+      
+      <TextInput
+         
+        value={password}
+        onChangeText={(newValue) => setPassword(newValue)}
+        autoCapitalize="none"
+        autoCorrect={false}
+        style={styles.input}
+      />
+      {password.length < 5 ? (
+        <Text>password must be longer than 5 characters </Text>
+      ) : null}
+      <Text>My Name is: {name}</Text>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    input:{
-        margin:15,
-        borderColor:'Black',
-        borderWidth:1
-    }
-})
+  input: {
+    margin: 15,
+    borderColor: "Black",
+    borderWidth: 1,
+  },
+});
 
-export default TextScreen
+export default TextScreen;
