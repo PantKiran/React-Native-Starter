fe86673e2616db0af1e6cb15782e982c5c13cf8f
kiran pant <kiran.r.pant@gmail.com>
2024-06-18 15:47:34 +0545

    UI:flex

--- src/screens/BoxScreen.jsx
+++ src/screens/BoxScreen.jsx
@@ -4,24 +4,47 @@ import { View, Text, StyleSheet } from "react-native";
 const BoxScreen = () => {
   return (
     <View style={styles.viewStyle}>
-      <Text style={styles.textStyle}>child #1</Text>
-      <Text style={styles.textStyle}>child #2</Text>
-      <Text style={styles.textStyle}>child #3</Text>
+      <Text style={styles.textOneStyle}>child #1</Text>
+      <Text style={styles.textTwoStyle}>child #2</Text>
+      <Text style={styles.textThreeStyle}>child #3</Text>
     </View>
   );
 };
+
+/*
+ parent :
+alignItem
+justifyContent
+flexDirection
+child:
+flex
+alignSelf
+
+*/
 
 const styles = StyleSheet.create({
   viewStyle: {
     borderWidth: 3,
     borderColor: "black",
     height: 200,
-    flexDirection: "row",
-    justifyContent: "flex-start",
+    alignItems:'center'
+   
   },
-  textStyle: {
+  textOneStyle: {
     borderWidth: 3,
     borderColor: "red",
+    
+  },
+  textTwoStyle: {
+    borderWidth: 3,
+    borderColor: "red",
+    alignSelf:'stretch'
+
+  }, 
+  textThreeStyle: {
+    borderWidth: 3,
+    borderColor: "red",
+    
   },
 });
 
